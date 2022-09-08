29ecc909197c0ab26bb585346eec82ed288deaf0
Felix Tovar <ftovar@balearesgroup.com>
2022-09-08 11:42:29 -0400

    launch

---
+++ config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  "package": "@beyond-examples/ssr",
+  "version": "0.0.1",
+  "languages": {
+    "default": "en",
+    "supported": ["en", "es"]
+  },
+  "local": "ssr.build"
+};
+exports.default = _default;
