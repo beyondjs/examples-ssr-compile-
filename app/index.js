c39e01835de6438cb02856005b49340f9ebbc343
Felix Tovar <ftovar@balearesgroup.com>
2022-09-08 11:39:52 -0400

    clean

--- app/index.js
+++
@@ -1,5 +0,0 @@
-globalThis.__app_package = '@beyond-examples/ssr';
-Object.defineProperty(globalThis, 'brequire', {get: () => require});
-Object.defineProperty(globalThis, 'bimport', {get: () => async specifier => require(specifier)});
-
-require('./start.js');
